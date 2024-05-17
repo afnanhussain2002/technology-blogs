d1e05bc4b3a4be6d1e928b6ec9cc805a77356265
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-05-17 07:18:16 +0600

    Style: Setup tailwind css and make header part

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    return (
+        <div className="border-b">
+            <div className="flex justify-between items-center p-3">
+                <h3 className="text-3xl font-semibold">Coading World</h3>
+                <img className="w-10 rounded-lg" src="https://2.bp.blogspot.com/-y2__wbIi5tQ/UX_XOzg3AWI/AAAAAAAANRk/pVT1VQ_J2xQ/s1600/5602.jpg" alt="photo" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
